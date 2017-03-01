b2d21fe755e37afb22de9a899f1932e9b7d5bc53
haiatto <haiattocc@gmail.com>
2017-03-02 01:59:34 +0900

    first commit

---
+++ NetworkTest/Assets/NetworkTest/NetworkSettning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSettning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var man = GetComponent<NetworkManager>();
+#if UNITY_WSA
+        man.StartHost();
+#else
+        man.StartClient();
+#endif
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        
+
+    }
+}
